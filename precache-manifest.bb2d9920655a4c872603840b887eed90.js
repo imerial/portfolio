e9085c7d19ace24bc3946f0d27cc63565d01998e
imerial <imerial@me.com>
2020-09-11 21:33:51 -0400

    Updates

---
+++ precache-manifest.bb2d9920655a4c872603840b887eed90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21dc9abe6d5071b57dd3f6ae5bf0b10f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "82869e8c63453e07671d",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/portfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82869e8c63453e07671d",
+    "url": "/portfolio/static/js/main.4aa8a6fb.chunk.js"
+  },
+  {
+    "revision": "d6ca8614ab4df5b1bf68",
+    "url": "/portfolio/static/js/runtime-main.44645aae.js"
+  }
+]);
